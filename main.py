55a183c726fb74933b655f0dcee6a52c62e3bb84
tomato-project <kkenya13@gmail.com>
2019-01-27 23:24:37 +0900

    ver0.0.2

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ soup = BeautifulSoup(r.text, 'html.parser')
 results = soup.find_all('tr')
 #<table class="first"> <tbody> <tr> <th> <a> </a> </th> </tr> </tbody> </table>
 
+
 def create_dataframe(results,keyword):
     table = []
     for a in results:
@@ -16,12 +17,17 @@ def create_dataframe(results,keyword):
         table.append(row)
     return table
 
-issuer = pd.DataFrame(create_dataframe(results,'th'), columns=["発行体","発行時期", "発行金額","資金使途","利率","償還期間"]).loc[:,['発行体']]
-table = pd.DataFrame(create_dataframe(results,'td'), columns=["発行時期", "発行金額","資金使途","利率","償還期間"])
 
-result = pd.concat([issuer, table], axis=1, sort=False)
-clean_df = result[~result["発行体"].isin(["発行体"])]
+def run():
+    issuer = pd.DataFrame(create_dataframe(results,'th'), columns=["発行体","発行時期", "発行金額","資金使途","利率","償還期間"]).loc[:, ['発行体']]
+    table = pd.DataFrame(create_dataframe(results,'td'), columns=["発行時期", "発行金額","資金使途","利率","償還期間"])
 
-clean_df.to_csv('masterfile.csv', encoding="Shift-JIS")
+    result = pd.concat([issuer, table], axis=1, sort=False)
+    clean_df = result[~result["発行体"].isin(["発行体"])]
 
-print('Done. check the output csv ("masterfile.csv")')
+    return clean_df.to_csv('masterfile.csv', encoding="Shift-JIS")
+
+
+if __name__ == '__main__':
+    run()
+    print('Done. check the output csv ("masterfile.csv")')
